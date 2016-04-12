4d880edc7f1d58a00f86606df87d2aa04af24de1
kasabali1122 <piratekemal@gmail.com>
2016-04-12 16:54:13 +0300

    reorder to export file

--- Dssem/Dssem/exportFile.Designer.cs
+++ Dssem/Dssem/exportFile.Designer.cs
@@ -35,6 +35,8 @@
             this.label5 = new System.Windows.Forms.Label();
             this.choseData = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // choseAdress
@@ -46,7 +48,7 @@
             "OCT",
             "DEC",
             "UNS"});
-            this.choseAdress.Location = new System.Drawing.Point(254, 70);
+            this.choseAdress.Location = new System.Drawing.Point(156, 65);
             this.choseAdress.Name = "choseAdress";
             this.choseAdress.Size = new System.Drawing.Size(121, 24);
             this.choseAdress.TabIndex = 3;
@@ -58,16 +60,17 @@
             this.choseFile.Items.AddRange(new object[] {
             ".mif",
             ".hex"});
-            this.choseFile.Location = new System.Drawing.Point(254, 175);
+            this.choseFile.Location = new System.Drawing.Point(156, 170);
             this.choseFile.Name = "choseFile";
             this.choseFile.Size = new System.Drawing.Size(121, 24);
             this.choseFile.TabIndex = 4;
             this.choseFile.Text = ".mif";
+            this.choseFile.SelectedIndexChanged += new System.EventHandler(this.choseFile_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(110, 70);
+            this.label3.Location = new System.Drawing.Point(12, 65);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(121, 17);
             this.label3.TabIndex = 7;
@@ -76,7 +79,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(129, 127);
+            this.label4.Location = new System.Drawing.Point(31, 122);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(102, 17);
             this.label4.TabIndex = 8;
@@ -85,7 +88,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(142, 178);
+            this.label5.Location = new System.Drawing.Point(44, 173);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(44, 17);
             this.label5.TabIndex = 9;
@@ -100,7 +103,7 @@
             "OCT",
             "DEC",
             "UNS"});
-            this.choseData.Location = new System.Drawing.Point(254, 124);
+            this.choseData.Location = new System.Drawing.Point(156, 119);
             this.choseData.Name = "choseData";
             this.choseData.Size = new System.Drawing.Size(121, 24);
             this.choseData.TabIndex = 10;
@@ -108,7 +111,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(193, 257);
+            this.button1.Location = new System.Drawing.Point(194, 259);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 53);
             this.button1.TabIndex = 11;
@@ -116,11 +119,27 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(382, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(382, 134);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 13;
+            // 
             // exportFile
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 349);
+            this.ClientSize = new System.Drawing.Size(544, 349);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.choseData);
             this.Controls.Add(this.label5);
@@ -143,5 +162,7 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox choseData;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
--- Dssem/Dssem/exportFile.cs
+++ Dssem/Dssem/exportFile.cs
@@ -14,7 +14,7 @@ namespace Dssem
     public partial class exportFile : Form
     {
         DSSM dssm = new DSSM();
-        int count = 0;
+        int count = 12;
         
         public exportFile(DSSM dssm)
         {
@@ -28,8 +28,8 @@ namespace Dssem
             if (choseFile.Text==".mif")
             {
                 FileWriter("16","8",".mif","C");
-                FileWriter("16", "8", ".mif", "D");
-                FileWriter("8", "8", ".mif", "S");
+              //  FileWriter("16", "8", ".mif", "D");
+             //   FileWriter("8", "8", ".mif", "S");
 
 
             }
@@ -41,46 +41,69 @@ namespace Dssem
 
         public void FileWriter(string depth,string length,string filetype,string memorytype)
         {
+
             string path = "Example"+Convert.ToString(count)+".mif";
             count++;
-            if (!File.Exists(path))
+            try
             {
-                File.Create(path);
-                TextWriter tw = new StreamWriter(path);
-                tw.WriteLine("DEPTH="+depth+";");
-                tw.WriteLine("WIDTH=" + length + ";");
-                tw.WriteLine("ADRESS_RADIX="+choseAdress.Text+";");
-                tw.WriteLine("DATA_RADIX=" + choseData.Text + ";");
-                tw.WriteLine("CONTENT");
-                tw.WriteLine("BEGIN");
-                if (memorytype=="C")
-                {
-                    for (int i = 0; i < Convert.ToInt32(depth); i++)
+                
+                   // File.Create(path);
+                    MessageBox.Show("GELDİ");
+                    FileStream fs1 = new FileStream("Example12.mif", FileMode.OpenOrCreate, FileAccess.Write);
+                    StreamWriter tw = new StreamWriter(fs1);
+                  
+                    tw.WriteLine("DEPTH=" + depth + ";");
+                    tw.WriteLine("WIDTH=" + length + ";");
+                    tw.WriteLine("ADRESS_RADIX=" + choseAdress.Text + ";");
+                    tw.WriteLine("DATA_RADIX=" + choseData.Text + ";");
+                    tw.WriteLine("CONTENT");
+                    tw.WriteLine("BEGIN");
+                    if (memorytype == "C")
                     {
-                        tw.Write(Util.convert(Convert.ToString(i),"DEC",choseAdress.Text)+":");
-                        tw.WriteLine(Util.convert(dssm.codeSegment[i].i + dssm.codeSegment[i].opcode + dssm.codeSegment[i].data,"BIN",choseData.Text) +";");
+                        for (int i = 0; i < Convert.ToInt32(depth); i++)
+                        {
+                            tw.Write(Util.convert(Convert.ToString(i), "DEC", choseAdress.Text) + ":");
+                            tw.WriteLine(Util.convert(dssm.codeSegment[i].i + dssm.codeSegment[i].opcode + dssm.codeSegment[i].data, "BIN", choseData.Text) + ";");
+                        }
                     }
-                }
-                else if (memorytype == "D")
-                {
-                    for (int i = 0; i < Convert.ToInt32(depth); i++)
+                    else if (memorytype == "D")
                     {
-                        tw.Write(Util.convert(Convert.ToString(i), "DEC", choseAdress.Text) + ":");
-                        tw.WriteLine(Util.convert(dssm.dataSegment[i].i + dssm.dataSegment[i].opcode + dssm.dataSegment[i].data, "BIN", choseData.Text) + ";");
+                        for (int i = 0; i < Convert.ToInt32(depth); i++)
+                        {
+                            tw.Write(Util.convert(Convert.ToString(i), "DEC", choseAdress.Text) + ":");
+                            tw.WriteLine(Util.convert(dssm.dataSegment[i].i + dssm.dataSegment[i].opcode + dssm.dataSegment[i].data, "BIN", choseData.Text) + ";");
+                        }
                     }
-                }
-                else if (memorytype == "S")
-                {
-                    for (int i = 0; i < Convert.ToInt32(depth); i++)
+                    else if (memorytype == "S")
                     {
-                        tw.Write(Util.convert(Convert.ToString(i), "DEC", choseAdress.Text) + ":");
-                        tw.WriteLine(Util.convert(dssm.stackSegment[i].i + dssm.stackSegment[i].opcode + dssm.stackSegment[i].data, "BIN", choseData.Text) + ";");
+                        for (int i = 0; i < Convert.ToInt32(depth); i++)
+                        {
+                            tw.Write(Util.convert(Convert.ToString(i), "DEC", choseAdress.Text) + ":");
+                            tw.WriteLine(Util.convert(dssm.stackSegment[i].i + dssm.stackSegment[i].opcode + dssm.stackSegment[i].data, "BIN", choseData.Text) + ";");
+                        }
                     }
-                }
 
-                tw.Close();
+                    tw.Close();
+
+                
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("File error");
             }
         }
 
+        private void choseFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (choseFile.Text==".mif")
+            {
+                
+            }
+            else
+            {
+
+            }
+        }
     }
 }
